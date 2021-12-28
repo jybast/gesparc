cd1c1404a878e25f28422044a7eba6be339f3bb9
Jean-Yves BASTARD <bastard.jeanyves@gmail.com>
2021-12-28 22:25:08 +0100

    20211228 Installation et mise en place authentification

---
+++ README.md
@@ -0,0 +1,14 @@
+# Gesparc
+## Gestion de parc automobile
+
+## Supports
+### Framework
+Symfony 6.0.1
+
+### Langages
+PHP 8.1
+HTML 5
+CSS 3
+SASS
+Javascript
+
---
+++ src/Controller/HomeController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    #[Route('/home', name: 'app_home')]
+    public function index(): Response
+    {
+        return $this->render('home/home.html.twig', [
+            'controller_name' => 'HomeController',
+        ]);
+    }
+
+    #[Route('/contact', name: 'app_contact')]
+    public function contact(): Response
+    {
+        return $this->render('home/contact.html.twig', [
+            'controller_name' => 'HomeController',
+        ]);
+    }
+
+    #[Route('/about', name: 'app_about')]
+    public function about(): Response
+    {
+        return $this->render('home/about.html.twig', [
+            'controller_name' => 'HomeController',
+        ]);
+    }
+}
